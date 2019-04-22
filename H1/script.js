037e180b255558a2ad29fb9ace08ae61a1b772be
Dmitry <msianya.q@gmail.com>
2019-04-22 21:33:51 +0300

    Первое задание

---
+++ H1/script.js
@@ -0,0 +1,27 @@
+﻿
+
+let money = prompt("Введите Ваш бюджет на месяц:", "");
+let time = prompt("Введите дату в формате YYYY-MM-DD:", "");
+let appData = {
+	budget: money,
+	timeData: time,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: false
+};
+
+for (i = 0; i < 2; i++){
+	var a = prompt("Статья расходов", "");
+	var b = prompt("Во сколько обойдется?", "");
+	appData.expenses[a] = b;
+};
+
+alert("Бюджет на один день составляет: " + (appData.budget / 30));
+
+/*Ответы на вопросы к заданию:
+	1)Исходя из Вашей лекции, в JS существует 7 типов данных:
+number, string, boolean, null, undefined, symbol, object.
+	2)Информация в консоль выводится через console.log("здесь 
+информация для вывода в консоль");
+	3)|| - логическое ИЛИ, && - логическое И.*/
